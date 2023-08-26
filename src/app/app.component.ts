aa2a8550b65752e33a8558fe10a80ff90cce03ef
unknown <noursmida28@gmail.com>
2023-08-26 11:36:17 +0200

    correction carousel css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { AppService } from './app.service';
-
+import { Component , AfterViewInit } from '@angular/core';
+import 'owl.carousel';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  
- 
-  skils: any
+export class AppComponent  implements AfterViewInit{
+  title = 'portofolio-angular';
 
-  constructor(private AppServie: AppService) { }
-
-    ngOnInit() {
-
-      this.skils = this.AppServie.skils()}}
- 
- 
-
+  ngAfterViewInit() {
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      nav: true,
+      items: 1
+    });
+  }
+}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppService } from './app.service';
+
 import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkilsComponent } from './skils/skils.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ResumeComponent,
+    ContactComponent,
+    SkilsComponent
   ],
   imports: [
     BrowserModule,
